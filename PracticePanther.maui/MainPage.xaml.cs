2987a7179ca030918e018220ce6b7e1236c473b0
Nicolas Azzi <azzi2003@outlook.com>
2023-06-10 21:32:34 -0400

    Added View Model

--- PracticePanther.maui/MainPage.xaml.cs
+++ PracticePanther.maui/MainPage.xaml.cs
@@ -1,26 +1,17 @@
-﻿namespace PracticePanther.maui
+﻿using PracticePanther.maui.ViewModels;
+
+namespace PracticePanther.maui
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
-            BindingContext = this;
+            BindingContext = new MainViewModel();
 
 
         }
 
-        public string Hello { get
-            {
-                return "Hello, world!";
-            } 
-        }
-
-        public string Interface { get
-            {
-                return "THIS WILL BE INTERFACE";
-            }
-        }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
---
+++ PracticePanther.maui/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticePanther.maui.ViewModels
+{
+    public class MainViewModel
+    {
+
+        public string Hello
+        {
+            get
+            {
+                return "Hello, world!";
+            }
+        }
+
+        public string Interface
+        {
+            get
+            {
+                return "THIS WILL BE INTERFACE";
+            }
+        }
+    }
+}
